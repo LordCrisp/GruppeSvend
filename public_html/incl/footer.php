7975c91e3ca0bb5fb4de567142a260ce3a45e7b0
LordCrisp <lord_crisp@hotmail.com>
2018-04-03 13:36:49 +0200

    FILER! :unicorn:

---
+++ public_html/incl/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer (start) -->
+<footer>
+
+</footer>
+<!-- Footer (end) -->
+
+<!-- Scripts (start) -->
+
+<!-- Scripts (end) -->
--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,13 @@
-// index
+<?php
+require "incl/init.php";
+/* - Head, Header & Body (start) - */
+require DOCROOT . "incl/header.php";
+?>
+
+<main>Fisk</main>
+
+
+<?php
+/* - Footer & Body (end) - */
+require DOCROOT . "incl/footer.php";
+?>
