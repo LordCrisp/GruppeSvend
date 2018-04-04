ee32bf3ce0de231165b2877195e8288b8d431794
LordCrisp <lord_crisp@hotmail.com>
2018-04-04 10:55:30 +0200

    lavet lidt :unicorn:

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,30 @@ require "incl/init.php";
 require DOCROOT . "/incl/header.php";
 ?>
 
-<main>Fisk</main>
+<main>
+    <section class="home__news--main">
+        <figure class="home__news--figure">
+            <picture class="home__news--picture">
+                <source srcset="assets/img/news1.jpg" media="(min-width: 650px)">
+                <source srcset="assets/img/news1.jpg" media="(min-width: 465px)">
+                <img src="assets/img/news1.jpg" alt="News picture">
+            </picture>
+        </figure>
+        <h2>LATEST ARRIVALS</h2>
+        <h5>Check our latest products here</h5>
+        <hr>
+        <?php
+        /* - Latest Products - */
+        $sql = "SELECT * FROM product LIMIT 6";
+        foreach ($db->query($sql) as $key => $values){
+            echo "<figure>
+                <img src='$key[thumbnail]' alt='Picture of $key[name]'>
+                <figcaption>$key[name]</figcaption>
+            </figure>";
+        }
+        ?>
+    </section>
+</main>
 
 
 <?php
