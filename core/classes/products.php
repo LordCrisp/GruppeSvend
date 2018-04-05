88b9bb08b009372df501a15284254eff3915f602
MathiasMogensen <mathiasgulbaek@hotmail.dk>
2018-04-05 09:19:56 +0200

    collections & products xd ;)

--- core/classes/products.php
+++ core/classes/products.php
@@ -8,12 +8,24 @@ class products {
         global $db;
         $this->db = $db;
     }
+    public function getProducts() {
+        $sql = "SELECT * FROM product";
+        return $this->db->fetch_array($sql);
+    }
     public function getLatestProducts() {
-        $sql = "SELECT * FROM product WHERE deleted = 0 ORDER BY id DESC LIMIT 6";
+        $sql = "SELECT * FROM product WHERE deleted = 0 ORDER BY created_at DESC LIMIT 6";
         return $this->db->fetch_array($sql);
     }
     public function getRandomProducts() {
         $sql = "SELECT * FROM product WHERE deleted = 0 ORDER BY RAND() LIMIT 5";
         return $this->db->fetch_array($sql);
+    }
+    public function getCollectionProducts($collection, $category) {
+        $params = array(
+            $collection,
+            $category
+        );
+        $sql = "SELECT * FROM product WHERE collection_id = ? AND category_id = ?";
+        return $this->db->fetch_array($sql, $params);
     }
 }
