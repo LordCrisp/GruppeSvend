3ba3e0ad543818feb46e50e0978ca8c43dd1fc6d
SanderSalamander <sander.salamander.larsen@gmail.com>
2018-04-10 10:06:28 +0200

    added untracked files to commit :happy:

---
+++ public_html/incl/search.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'init.php';
+
+$products = new Products();
+$searchResult = $products->searchProducts($_POST['search']);
+
+foreach ($searchResult as $product) : ?>
+  <li>
+    <figure>
+      <img src="assets/img/products/<?=$product['thumbnail']?>" alt="Picture of <?=$product['name']?>" />
+    </figure>
+    <figcaption>
+      <p><?=$product['name']?></p>
+      <a href="details.php?product=<?=$product['id']?>">More ></a>
+    </figcaption>
+  </li>
+<?php endforeach; ?>
