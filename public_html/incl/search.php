d3925f05979ca4bc68fa791c11085d2da3f3afdd
MathiasMogensen <mathiasgulbaek@hotmail.dk>
2018-04-10 13:59:44 +0200

    lol :shit:

--- public_html/incl/search.php
+++ public_html/incl/search.php
@@ -4,7 +4,34 @@ require 'init.php';
 
 $products = new Products();
 $searchResult = $products->searchProducts($_POST['search']);
-foreach ($searchResult as $product) : ?>
+$searchResultCategory = $products->searchCategories($_POST['search']);
+$searchResultCollection = $products->searchCollections($_POST['search']);
+?>
+<?php foreach ($searchResultCollection as $collection) : ?>
+  <li>
+    <figure>
+      <img src="assets/img/collections/big/<?=$collection['thumbnail']?>" alt="Picture of <?=$collection['name']?>" />
+    </figure>
+    <figcaption>
+      <p><?=$collection['name']?></p>
+      <a href="details.php?product=<?=$collection['id']?>">More ></a>
+    </figcaption>
+  </li>
+<?php endforeach; ?>
+
+<?php foreach ($searchResultCategory as $category) : ?>
+  <li>
+    <figure>
+      <img src="assets/img/categories/<?=$category['thumbnail']?>" alt="Picture of <?=$category['name']?>" />
+    </figure>
+    <figcaption>
+      <p><?=$category['name']?></p>
+      <a href="details.php?product=<?=$category['id']?>">More ></a>
+    </figcaption>
+  </li>
+<?php endforeach; ?>
+
+<?php foreach ($searchResult as $product) : ?>
   <li>
     <figure>
       <img src="assets/img/products/<?=$product['thumbnail']?>" alt="Picture of <?=$product['name']?>" />
