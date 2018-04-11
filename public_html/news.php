b91aca08030822810edb12f935ff9befb879b952
SanderSalamander <s4nd3rs4l4m4nd3r@gmail.com>
2018-04-11 20:09:34 +0200

    Added some markup to new.php :unicorn:

--- public_html/news.php
+++ public_html/news.php
@@ -3,3 +3,17 @@ require "incl/init.php";
 /* - Head, Header & Body (start) - */
 require DOCROOT . "incl/header.php";
 ?>
+
+<main class="alt__container">
+    <header>
+        <div>
+            <h1>News</h1>
+            <small>Here you can find the latest news</small>
+        </div>
+        <div></div>
+    </header>
+</main>
+
+<?php
+require 'incl/footer.php';
+?>
