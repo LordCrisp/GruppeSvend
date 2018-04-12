dcef6bb63d0c10ce4f46899e891abe55f9b96de5
SanderSalamander <sander.salamander.larsen@gmail.com>
2018-04-12 13:07:37 +0200

    :)

--- _FILES/SQL/kuntilsteffen.sql
+++ _FILES/SQL/kuntilsteffen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 12, 2018 at 01:03 PM
+-- Generation Time: Apr 12, 2018 at 01:07 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -22,16 +22,29 @@ SET time_zone = "+00:00";
 -- Database: `gruppesvend`
 --
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `category`
+-- Table structure for table `user`
 --
 
-INSERT INTO `category` (`id`, `name`, `thumbnail`) VALUES
-(1, 'shoes', ''),
-(2, 'pants', ''),
-(3, 't-shirts', ''),
-(4, 'tops', ''),
-(5, 'accessories', '');
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `deleted` tinyint(1) NOT NULL,
+  `salt` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `password`, `address`, `role_id`, `deleted`, `salt`) VALUES
+(2, 'admin', '$2y$10$0ee5M0LFrNPaCk30r2WVee28Wka0m/BgZwZEaTDdomPRpl25ZDw.u', '', 1, 0, ''),
+(3, 'retailer', 'retailer', '', 2, 0, '');
 
 -- --------------------------------------------------------
 
@@ -57,6 +70,37 @@ INSERT INTO `user_session` (`session_id`, `user_id`, `logged_in`, `last_action`)
 ('3fce4102a5f4df87783bc04853b6329f', 2, 1, 1523432480),
 ('28eb163a01680ea61c17fc71d21ff506', 2, 0, 1523521112),
 ('28eb163a01680ea61c17fc71d21ff506', 2, 0, 1523521120);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
