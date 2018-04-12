b4dccaccdb82d37dba37d999a3f04afb42643fd4
SanderSalamander <sander.salamander.larsen@gmail.com>
2018-04-12 14:58:15 +0200

    Added things

--- public_html/cms/incl/header.php
+++ public_html/cms/incl/header.php
@@ -5,9 +5,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="theme-color" content="#2D2D2D" id="theme_color">
 	<title>Admin | Fashion Online</title>
+
 	<!-- Custom stylesheet -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="/assets/css/master.css">
+	<link rel="stylesheet" href="https://cdn.rawgit.com/SanderSalamander/md-admin/master/css/master.css">
+	<link rel="stylesheet" href="/cms/assets/css/master.css">
 	<!-- Google fonts -->
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!-- Favicon -->
@@ -21,14 +22,33 @@
 </head>
 
 <!-- Body (start) -->
-<body class="admin__body">
+<body>
 <!-- Header (start) -->
-<header>
+<div id="sidebarToggle" style="display:none"></div>
+<div id="overlay" style="display: none"></div>
+<nav class="header__sidebar" id="sidebar">
+	<div class="header__title">
+		<h1 class="header__brand">CMS</h1>
+	</div>
+	<ul class="header__list">
+		<li><a href="index.php"><i class="material-icons">home</i><span>Home</span></a></li>
 	<?php if ($auth->auth_role == 'admin') : ?>
+		<li><a href="/cms/products.php"><i class="material-icons">shopping_basket</i><span>Products</span></a></li>
+	<?php elseif ($auth->auto_role == 'retailer') : ?>
+		<li><a href="/cms/retail_adress.php"><i class="material-icons">pin_drop</i><span>Change address</span></a></li>
+	<?php endif; ?>
+	</ul>
+	<ul class="header__list">
+		<li><a href="?action=logout"><i class="material-icons" style="transform:rotate(180deg)">exit_to_app</i><span>Log out</span></a></li>
+	</ul>
+</nav>
+<header class="header__top--fixed header--style-primary" id="appBar">
+</header>
+
+	<!-- <?php if ($auth->auth_role == 'admin') : ?>
 		<a href="/cms/products.php">products</a>
 	<?php elseif ($auth->auth_role == 'retailer') : ?>
 		<a href="/cms/retail_adress.php">Change address</a>
 	<?php endif; ?>
-	<a href="?action=logout">Logout</a>
-</header>
+	<a href="?action=logout">Logout</a> -->
 <!-- Header (end) -->
--- public_html/cms/products.php
+++ public_html/cms/products.php
@@ -25,10 +25,23 @@ switch(strtoupper($mode)) {
     ORDER BY created_at ASC";
     $products = $db->fetch_array($sql);
 ?>
-    <div class="container">
-    <a href="?mode=edit">Create Product</a>
-        <table class="table">
+    <div class="container card"style="padding-left:16px">
+    <div class="table__header">
+      <ul class="header__list--left">
+        <li><h2 class="header__title">Products</h2></li>
+      </ul>
+      <ul class="header__list--right">
+        <li><a href="?mode=edit" class="button__icon"><i class="material-icons">add</i></a></li>
+      </ul>
+      <div class="header__contextual" id="contextual">
+        <ul class="header__list--left">
+          <p></p>
+        </ul>
+      </div>
+    </div>
+        <table class="table--embedded">
             <thead>
+                <!-- <th class="row__select"><div class="checkbox__group"><input type="checkbox" class="checkbox checkbox-master" data-contextual="contextual" /></div></th> -->
                 <th scope="col">ID</th>
                 <th scope="col">Name</th>
                 <th scope="col">Category</th>
@@ -36,16 +49,17 @@ switch(strtoupper($mode)) {
             <?php foreach ($products as $product) : ?>
                 <tbody>
                     <tr scope="row">
+                        <!-- <td class="row__select"><div class="checkbox__group"><input type="checkbox" class="checkbox" data-contextual="contextual" /></div></td> -->
                         <td><?=$product['id']?></td>
                         <td><?=$product['productName']?></td>
                         <td><?=$product['categoryName']?></td>
-                        <td><a href="?mode=edit&id=<?=$product['id']?>">Edit</a></td>
-                        <td><a href="?mode=delete&id=<?=$product['id']?>"><span class="product-delete" id="<?=$product['productName']?>">Slet</span></a></td>
+                        <td><a href="?mode=edit&id=<?=$product['id']?>"><i class="material-icons">edit_mode</i></a>
+                        <a href="?mode=delete&id=<?=$product['id']?>"><span class="product-delete" id="<?=$product['productName']?>"><i class="material-icons">delete</i></span></a></td>
                     </tr>
                 </tbody>
             <?php endforeach; ?>
         </table>
-    </div>
+    </>
 <script>
 $(document).ready(function() {
     $(".product-delete").click(function() {
@@ -61,52 +75,54 @@ break;
     require DOCROOT . "/cms/incl/header.php";
 ?>
 <!-- Form (start) -->
-<?php 
+<?php
     $collections = $collections->getCollections();
 	$categories = $categories->getCategories();
     if (isset($_GET['id'])) {
         $product->getProduct($_GET['id']);
     }
 ?>
-    <form action="?mode=save" method="POST" enctype="multipart/form-data">
-        <input type="hidden" name="id" value="<?=$product->id?>">
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" name="name" value="<?=$product->name?>">
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label><br>
-            <textarea name="description" rows="5" cols="40"><?=$product->description?></textarea>
-        </div>
-        <div class="form-group">
-            <label for="collection">Collection</label>
-            <select name="collection">
-                <?php foreach ($collections as $collection) : ?>
-                    <option value="<?=$collection['id']?>"><?=$collection['name']?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="gender">gender</label>
-            <select name="gender">
-                <option value="1">Male</option>
-                <option value="2">Female</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="category">Category</label>
-            <select name="category">
-                <?php foreach ($categories as $category) : ?>
-                    <option value="<?=$category['id']?>"><?=$category['name']?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-        <div class="form-group">
-            <input type="file" name="file" accept="image/*">
-        </div>
-        <input type="submit" name="submit">
-    </form>
-<!-- Form (end) -->
+    <div class="container card">
+      <form action="?mode=save" method="POST" enctype="multipart/form-data">
+          <input type="hidden" name="id" value="<?=$product->id?>">
+          <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" name="name" value="<?=$product->name?>">
+          </div>
+          <div class="form-group">
+              <label for="description">Description</label><br>
+              <textarea name="description" rows="5" cols="40"><?=$product->description?></textarea>
+          </div>
+          <div class="form-group">
+              <label for="collection">Collection</label>
+              <select name="collection">
+                  <?php foreach ($collections as $collection) : ?>
+                      <option value="<?=$collection['id']?>"><?=$collection['name']?></option>
+                  <?php endforeach; ?>
+              </select>
+          </div>
+          <div class="form-group">
+              <label for="gender">gender</label>
+              <select name="gender">
+                  <option value="1">Male</option>
+                  <option value="2">Female</option>
+              </select>
+          </div>
+          <div class="form-group">
+              <label for="category">Category</label>
+              <select name="category">
+                  <?php foreach ($categories as $category) : ?>
+                      <option value="<?=$category['id']?>"><?=$category['name']?></option>
+                  <?php endforeach; ?>
+              </select>
+          </div>
+          <div class="form-group">
+              <input type="file" name="file" accept="image/*">
+          </div>
+          <input type="submit" name="submit">
+      </form>
+    </div>
+  <!-- Form (end) -->
 
 <?php
 break;
@@ -114,7 +130,7 @@ break;
     //  DELETE PRODUCT
     case "DELETE":
     require DOCROOT . "/cms/incl/header.php";
-    
+
     if (isset($_GET['id'])) {
         $product->delete($_GET['id']);
         header("Location: ?mode=list");
@@ -130,7 +146,7 @@ break;
     $product->collection = $_POST['collection'];
     $product->category = $_POST['category'];
     $product->gender = $_POST['gender'];
-    
+
     if (!empty($_POST['id'])) {
         $product->save($product->id, 'assets/img/products/');
     } else {
@@ -141,7 +157,9 @@ break;
 }
 ?>
 
-<?php else : 
-    header("Location: index.php");    
+<?php else :
+    header("Location: index.php");
 ?>
 <?php endif; ?>
+
+	<script src="https://cdn.rawgit.com/SanderSalamander/md-admin/master/scripts/mdjs.js"></script>
